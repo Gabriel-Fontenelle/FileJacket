aac9148898e40c774a6f009b602bec228f7c9bc3
Gabriel Fontenelle <contato@gabrielfontenelle.com>
2025-04-27 00:15:41 +0200

    tests: Add test for checking if file is the same.

--- tests/unitary/pipelines/test_comparer.py
+++ tests/unitary/pipelines/test_comparer.py
@@ -28,15 +28,79 @@ from filejacket.pipelines.comparer import (
     ]
 )
 def test_class_for_comparing_has_required_attribute(package_class):
-    assert hasattr(package_class, 'prepare_filename')
-    assert hasattr(package_class, 'add_reserved_name')
-    assert hasattr(package_class, 'clean_reserved_names')
-    assert hasattr(package_class, 'register_error')
+    assert hasattr(package_class, 'is_the_same')
     assert hasattr(package_class, 'process')
-    assert hasattr(package_class, 'is_name_reserved')
-    assert hasattr(package_class, 'get_name')
 
 
-def test_base_class_for_comparing_raise_not_implemented_error_in_some_attributes(request, file_jpg, file_svg):
+def test_base_class_for_comparing_raise_not_implemented_error_in_some_attributes(file_jpg, file_svg):
     with pytest.raises(NotImplementedError):
         BaseComparer.is_the_same(file_1=file_jpg, file_2=file_svg)
+
+
+@pytest.mark.parametrize(
+    "package_class",
+    [
+        DataCompare,
+        SizeCompare,
+        HashCompare,
+        LousyNameCompare,
+        NameCompare,
+        MimeTypeCompare,
+        BinaryCompare,
+        TypeCompare
+    ]
+)
+def test_method_is_the_same_with_equal_file_should_return_true(package_class, file_jpg):
+    assert package_class.is_the_same(file_1=file_jpg, file_2=file_jpg)
+
+
+@pytest.mark.parametrize(
+    "package_class",
+    [
+
+        DataCompare,
+        SizeCompare,
+        HashCompare,
+        LousyNameCompare,
+        NameCompare,
+        MimeTypeCompare,
+        BinaryCompare,
+        TypeCompare
+    ]
+)
+def test_method_is_the_same_with_unequal_file_should_return_false(package_class, file_gif, file_txt):
+    assert package_class.is_the_same(file_1=file_gif, file_2=file_txt) is False
+
+
+@pytest.mark.parametrize(
+    "package_class",
+    [
+        DataCompare,
+        SizeCompare,
+        HashCompare,
+        LousyNameCompare,
+        NameCompare,
+        MimeTypeCompare,
+        BinaryCompare,
+        TypeCompare
+    ]
+)
+def test_method_process_with_equal_files_should_return_true(package_class, file_jpg):
+    assert package_class.process(object_to_process=file_jpg, objects_to_compare=[file_jpg])
+
+    
+@pytest.mark.parametrize(
+    "package_class",
+    [
+        DataCompare,
+        SizeCompare,
+        HashCompare,
+        LousyNameCompare,
+        NameCompare,
+        MimeTypeCompare,
+        BinaryCompare,
+        TypeCompare
+    ]
+)
+def test_method_is_the_same_with_unequal_files_should_return_false(package_class, file_jpg, file_txt):
+    assert package_class.process(object_to_process=file_jpg, objects_to_compare=[file_txt]) is False
